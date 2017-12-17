0e55d22d2e1cb7638d722669acb09cdd496e6ab5
dexter aparicio <truthadjustr@gmail.com>
2017-12-17 19:02:14 +0800

    add sendtrap() for failedlogon logic

--- pyhandlers/failedlogon_handler.py
+++ pyhandlers/failedlogon_handler.py
@@ -2,6 +2,27 @@ import redis
 import pysnmp
 import time
 import json
+from pysnmp.hlapi import *
+
+def sendtrap():
+    errorIndication, errorStatus, errorIndex, varBinds = next(
+        sendNotification(
+            SnmpEngine(),
+            CommunityData('public'),
+            UdpTransportTarget(('172.25.0.11',162)),
+            ContextData(),
+            'trap',
+            NotificationType(
+                ObjectIdentity('1.3.6.1.6.3.1.1.5.2')
+            ).addVarBinds(
+                ('1.3.6.1.6.3.1.1.4.3.0','1.3.6.1.4.1.20408.4.1.1.2'),
+                ('1.3.6.1.2.1.1.1.0',OctetString('my system'))
+            )
+        )
+    )
+
+    if errorIndication:
+        print(errorIndication)
 
 r = redis.StrictRedis(host = "cache")
 pubsub = r.pubsub()
@@ -22,3 +43,4 @@ while True:
             r.expire(k,60)
             if count == 4:
                 print("ALERT failed logon!")
+                sendtrap()
