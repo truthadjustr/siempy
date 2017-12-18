bcc5c603a3ea05e292b406b3babb7137cf576617
dexter aparicio <truthadjustr@gmail.com>
2017-12-18 10:38:19 +0800

    add dumy sendtrap_* body

--- pyhandlers/libs/sendtrap.py
+++ pyhandlers/libs/sendtrap.py
@@ -21,3 +21,105 @@ def sendtrap_failedlogon():
 
     if errorIndication:
         print(errorIndication)
+
+def sendtrap_foundrogueap():
+    errorIndication, errorStatus, errorIndex, varBinds = next(
+        sendNotification(
+            SnmpEngine(),
+            CommunityData('public'),
+            UdpTransportTarget(('172.25.0.3',162)),
+            ContextData(),
+            'trap',
+            NotificationType(
+                ObjectIdentity('1.3.6.1.6.3.1.1.5.2')
+            ).addVarBinds(
+                ('1.3.6.1.6.3.1.1.4.3.0','1.3.6.1.4.1.20408.4.1.1.2'),
+                ('1.3.6.1.2.1.1.1.0',OctetString('my system'))
+            )
+        )
+    )
+
+    if errorIndication:
+        print(errorIndication)
+
+def sendtrap_interference():
+    errorIndication, errorStatus, errorIndex, varBinds = next(
+        sendNotification(
+            SnmpEngine(),
+            CommunityData('public'),
+            UdpTransportTarget(('172.25.0.3',162)),
+            ContextData(),
+            'trap',
+            NotificationType(
+                ObjectIdentity('1.3.6.1.6.3.1.1.5.2')
+            ).addVarBinds(
+                ('1.3.6.1.6.3.1.1.4.3.0','1.3.6.1.4.1.20408.4.1.1.2'),
+                ('1.3.6.1.2.1.1.1.0',OctetString('my system'))
+            )
+        )
+    )
+
+    if errorIndication:
+        print(errorIndication)
+
+def sendtrap_lostrogueap():
+    errorIndication, errorStatus, errorIndex, varBinds = next(
+        sendNotification(
+            SnmpEngine(),
+            CommunityData('public'),
+            UdpTransportTarget(('172.25.0.3',162)),
+            ContextData(),
+            'trap',
+            NotificationType(
+                ObjectIdentity('1.3.6.1.6.3.1.1.5.2')
+            ).addVarBinds(
+                ('1.3.6.1.6.3.1.1.4.3.0','1.3.6.1.4.1.20408.4.1.1.2'),
+                ('1.3.6.1.2.1.1.1.0',OctetString('my system'))
+            )
+        )
+    )
+
+    if errorIndication:
+        print(errorIndication)
+
+def sendtrap_radius():
+    errorIndication, errorStatus, errorIndex, varBinds = next(
+        sendNotification(
+            SnmpEngine(),
+            CommunityData('public'),
+            UdpTransportTarget(('172.25.0.3',162)),
+            ContextData(),
+            'trap',
+            NotificationType(
+                ObjectIdentity('1.3.6.1.6.3.1.1.5.2')
+            ).addVarBinds(
+                ('1.3.6.1.6.3.1.1.4.3.0','1.3.6.1.4.1.20408.4.1.1.2'),
+                ('1.3.6.1.2.1.1.1.0',OctetString('my system'))
+            )
+        )
+    )
+
+    if errorIndication:
+        print(errorIndication)
+
+def sendtrap_webconfig():
+    errorIndication, errorStatus, errorIndex, varBinds = next(
+        sendNotification(
+            SnmpEngine(),
+            CommunityData('public'),
+            UdpTransportTarget(('172.25.0.3',162)),
+            ContextData(),
+            'trap',
+            NotificationType(
+                ObjectIdentity('1.3.6.1.6.3.1.1.5.2')
+            ).addVarBinds(
+                ('1.3.6.1.6.3.1.1.4.3.0','1.3.6.1.4.1.20408.4.1.1.2'),
+                ('1.3.6.1.2.1.1.1.0',OctetString('my system'))
+            )
+        )
+    )
+
+    if errorIndication:
+        print(errorIndication)
+
+
