a84884ab33752a01c591aaa8355cd1cdf434cd87
team viewer <teamviewer@user.com>
2017-12-26 09:44:42 +0000

    update

---
+++ pyhandlers/siemprocethspoof.py
@@ -0,0 +1,30 @@
+
+
+'''
+{
+      "@timestamp" => 2017-12-26T09:41:54.110Z,
+         "message" => "<105>  id=firewall sn=C0EAE4F5F08E time=\"2017-12-26 17:41:16\" fw=0.0.0.0 pri=1 c=32 gcat=6 m=1209 srcMac=08:00:27:43:1f:10 src=10.7.1.19:46897:X0 srcZone=LAN dstMac=c0:ea:e4:f5:f0:8e dst=165.21.83.88:53 proto=udp/dns rcvd=72 n=81708 fw_action=\"NA\"",
+              "x0" => "X0",
+            "time" => "2017-12-26 17:41:16",
+        "@version" => "1",
+              "id" => "firewall",
+              "fw" => "0.0.0.0",
+               "c" => "32",
+            "host" => "10.7.1.150",
+             "dst" => "165.21.83.88:53",
+           "proto" => "udp/dns",
+            "gcat" => "6",
+       "fw_action" => "NA",
+    "redischannel" => "siemprocethspoof",
+          "srcMac" => "08:00:27:43:1f:10",
+             "pri" => [
+        [0] "105",
+        [1] "1"
+    ],
+            "rcvd" => "72",
+             "src" => "10.7.1.19:46897",
+         "srcZone" => "LAN",
+               "n" => "81708",
+          "dstMac" => "c0:ea:e4:f5:f0:8e"
+}
+'''
